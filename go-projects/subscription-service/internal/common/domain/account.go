e69884f54b61875d7a19f976c39fc8622bc792e1
dolelongan <dolelongan@gmail.com>
2024-08-12 17:23:16 +0700

    feat: implement account related types

--- go-projects/subscription-service/internal/common/domain/account.go
+++ go-projects/subscription-service/internal/common/domain/account.go
@@ -7,14 +7,120 @@ import (
 
 type AccountNumber string
 
-type AccountBalances struct {
-	CurrentBalance float64
+// Account interface representing a generic account
+type Account interface {
+	GetAccountNumber() AccountNumber
+	GetBalance() float64
+	GetType() string
+	IsEligibleForCancellation() bool
+}
+
+// BaseAccount struct to be embedded in other account types
+type BaseAccount struct {
+	AccountNumber AccountNumber
+	Balance       float64
+}
+
+// GetAccountNumber Implement common methods for BaseAccount
+func (acc BaseAccount) GetAccountNumber() AccountNumber {
+	return acc.AccountNumber
+}
+
+func (acc BaseAccount) GetBalance() float64 {
+	return acc.Balance
+}
+
+// ActiveAccount struct representing an active account
+type ActiveAccount struct {
+	BaseAccount
+}
+
+func (acc ActiveAccount) GetType() string {
+	return "active"
+}
+
+func (acc ActiveAccount) IsEligibleForCancellation() bool {
+	// Add logic specific to ActiveAccount cancellation
+	return true
+}
+
+// TrialAccount struct representing a trial account
+type TrialAccount struct {
+	BaseAccount
+}
+
+func (acc TrialAccount) GetType() string {
+	return "trial"
+}
+
+func (acc TrialAccount) IsEligibleForCancellation() bool {
+	// Add logic specific to TrialAccount cancellation
+	return true
+}
+
+// SuspendedAccount struct representing a suspended account
+type SuspendedAccount struct {
+	BaseAccount
+}
+
+func (acc SuspendedAccount) GetType() string {
+	return "suspended"
+}
+
+func (acc SuspendedAccount) IsEligibleForCancellation() bool {
+	// Add logic specific to SuspendedAccount cancellation
+	return false
+}
+
+// CancelledAccount struct representing an account that has been cancelled but is still active within a grace period
+type CancelledAccount struct {
+	BaseAccount
+	GracePeriodEnd string // Example field for when the service actually stops
+}
+
+func (acc CancelledAccount) GetType() string {
+	return "cancelled"
+}
+
+func (acc CancelledAccount) IsEligibleForCancellation() bool {
+	// Generally, a cancelled account is already in the process of being canceled
+	// But you might have additional logic here
+	return false
+}
+
+// LegacyAccount struct representing a legacy account
+type LegacyAccount struct {
+	BaseAccount
+}
+
+func (acc LegacyAccount) GetType() string {
+	return "legacy"
+}
+
+func (acc LegacyAccount) IsEligibleForCancellation() bool {
+	// Add logic specific to LegacyAccount cancellation
+	return true
+}
+
+// FamilyOrEnterpriseAccount struct representing a family or enterprise account
+type FamilyOrEnterpriseAccount struct {
+	BaseAccount
+	Members int // Example field for number of members/users
+}
+
+func (acc FamilyOrEnterpriseAccount) GetType() string {
+	return "family_or_enterprise"
+}
+
+func (acc FamilyOrEnterpriseAccount) IsEligibleForCancellation() bool {
+	// Add logic specific to FamilyOrEnterpriseAccount cancellation
+	return true
 }
 
 // ExtractAccountNumberFrom
 //
 // TODO: This is a refactor from:
-// NewAccountNumberFromResourceName
+// NewAccountNumberFromResourceName()
 func ExtractAccountNumberFrom(resourceName string) AccountNumber {
 	accountPrefix := "accounts/"
 	return AccountNumber(strings.TrimPrefix(resourceName, accountPrefix))
@@ -23,4 +129,12 @@ func ExtractAccountNumberFrom(resourceName string) AccountNumber {
 func (acc AccountNumber) ResourceName() string {
 	accountPrefix := "accounts/"
 	return fmt.Sprintf("%s%s", accountPrefix, acc)
+}
+
+// IsEqual
+//
+// TODO: This is a refactor from:
+// Is
+func (acc AccountNumber) IsEqual(accountNumber string) bool {
+	return string(acc) == accountNumber
 }
