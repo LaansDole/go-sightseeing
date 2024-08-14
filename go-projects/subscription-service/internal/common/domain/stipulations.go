d4634ce5ea856e34567182dfd256920de25de377
dolelongan <dolelongan@gmail.com>
2024-08-14 14:02:13 +0700

    chore: change stipulation name

--- go-projects/subscription-service/internal/common/domain/stipulations.go
+++ go-projects/subscription-service/internal/common/domain/stipulations.go
@@ -4,12 +4,9 @@ type StipulationName string
 
 // Account Status Stipulations
 const (
-	StipulationNegativeBalance       StipulationName = "Account Status/Negative Balance"
-	StipulationActiveSubscription    StipulationName = "Account Status/Active Subscription"
-	StipulationNoActiveServices      StipulationName = "Account Status/No Active Services"
-	StipulationRefundProcessing      StipulationName = "Account Status/Refund Processing"
-	StipulationDeceasedAccountHolder StipulationName = "Account Status/Deceased Account Holder"
-	StipulationUnpaidService         StipulationName = "Account Status/Unpaid Service"
+	StipulationNegativeBalancePrediction StipulationName = "Account Status/Negative Balance Prediction"
+	StipulationActiveSubscription        StipulationName = "Account Status/Active Subscription"
+	StipulationDeceasedAccountHolder     StipulationName = "Account Status/Deceased Account Holder"
 )
 
 // Payment Arrangement Stipulations
